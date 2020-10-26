5d25055ed3289e9c76e28fca51d14e4fa775b97c
Nara Barros <narabarroscruz@gmail.com>
2020-10-26 09:57:18 -0300

    atualizados

--- cypress/integration/pages_specs/envio_campanha.spec.js
+++ cypress/integration/pages_specs/envio_campanha.spec.js
@@ -1,35 +1,52 @@
+require("cypress-xpath");
 /// <reference types="cypress" />
-
 import { emailGestor, senhaGestor } from '../../support/elements/login_po.js';
-import { baseUrl, baseUrlMaling } from '../../support/elements/urls_acessos.js';
+import { baseUrl, baseUrlMaling, baseUrlCampanha } from '../../support/elements/urls_acessos.js';
 
-describe('Envio de Ativo pelo sistema', () => {
+describe('Envio de Campanha pelo sistema', () => {
 
    beforeEach(() => {
       cy.visit(baseUrl);
       cy.loginGestor(emailGestor, senhaGestor);
-      cy.visit(baseUrlMaling);
+      //cy.visit(baseUrlMaling);
       //clica incluir um mailing
-      cy.get('#link_add_new_mailing').click();
+      //cy.get('#link_add_new_mailing').click();
       //preenche os dados para criar
-      cy.get('[id="title"]').type('testeRelease');
-      cy.get('#upload');
+      //cy.get('[id="title"]').type('testeRelease');
+      //cy.get('#upload');
    })
 
     it.only ('Enviar uma campanha pelo Whatsapp Web via Mailing', () => {
-    //Clicar no botão data-test="attendances-button-send_active" - precisa trocar pra campanha
-    cy.get('.QT-link-menu-config').click();
-    //clica no menu drop config
-    cy.get('[data-test="menu-a-config_campaign_list"]').click();
-    //clica no btn gestao de campanha
+    //Acessando URL campanha
+    cy.visit(baseUrlCampanha);
+    //clica no btn criar campanha
     cy.get('[class="btn btn-primary"]').click();
     //clica no btn criar campanha
-    cy.get('["class="form-control ng-pristine ng-empty ng-invalid ng-invalid-required ng-touched"]');
-    //- precisa inserir a busca aqui p selecionar o calendario c a data ---- incompleto
+    cy.xpath('/html/body/div[1]/section[1]/div[1]/form/div[1]/div[3]div/input').click();
+    //inspeciona calendario
+    cy.xpath('/html/body/div[7]/div[2]').each(($i,index,$list)=>{
+        var Date = $i.text()
+        if (Date == "15") {
+            cy.wrap($i).click()
+        }
+        //seleciona data de calendario (percorre todas as datas e seleciona a atual)
+    })
+
     cy.get('["value="string:waweb"]').select('string:waweb');
     //seleciona o canal whatsapp web
     cy.get('["mktzap_bt_action_campaign_modal_select_mailing"]').click();
     //clica no btn p inserir mailing
-    
+    cy.get('["mktzap_bt_action_campaign_select_mailing"]').click();
+    //clica no mailing e seleciona
+    cy.get('["mktzap_bt_action_campaign_add_text"]').click();
+    //clica no campo de texto
+    cy.get('["whatsapp-message-0"]').type("teste_campanha_release");
+    //clica no campo para preencher (quadrado)
+    cy.get('["mktzap_bt_action_campaign_save"]').click();
+    //clica no btn p salvar campanha
    })
+
+   //envio por base de contatos
+
+   cy.get('')
 })
--- cypress/support/elements/urls_acessos.js
+++ cypress/support/elements/urls_acessos.js
@@ -5,3 +5,4 @@ export const baseUrlLord = 'https://qa-poli.infranw.com.br/wolologin';
 export const baseUrlLogado ='https://qa-poli.infranw.com.br/chat';
 export const baseUrlLogout = 'https://qa-poli.infranw.com.br/login';
 export const baseUrlMaling = 'https://qa-poli.infranw.com.br/mailing-list';
+export const baseUrlCampanha = 'https://qa-poli.infranw.com.br/campaign-list';
