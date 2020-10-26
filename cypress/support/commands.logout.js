70172c70111c9eefaa2ec17572dbe878a6053f79
sandra.pavan@gruponewway.com.br <sandra.pavan@gruponewway.com.br>
2020-10-26 09:00:03 -0300

    alterando envio

--- cypress/support/commands.logout.js
+++ cypress/support/commands.logout.js
@@ -2,13 +2,9 @@ import { iconePerfil, logout } from "./elements/menu_profile_po";
 
 Cypress.Commands.add("logoutMktzap", () => {
 
-  cy.get(iconePerfil)
-    .click();
+  cy.get(iconePerfil).click();
 
-  cy.get(logout)
-    .click();
-
-
+  cy.get(logout).click();
 
 })
 
