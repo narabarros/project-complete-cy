e0cdbca839e33cdbac2f75715bd724801c98c6e7
sandra.pavan@gruponewway.com.br <sandra.pavan@gruponewway.com.br>
2020-11-17 17:50:44 -0300

    atualizando os elementos

--- cypress/support/elements/login.js
+++ cypress/support/elements/login.js
@@ -1,24 +1,33 @@
-/* LOGIN */
-//Acesso como Lord
-export const userNameLord = '[data-test="wolologin-input-user_name"]';
-export const userPasswordLord = '[data-test="wolologin-input-password"]';
-export const btnLoginLord = '[data-test="wolologin-input-login"]';
-export const emailLord = 'lord@email.com';
-export const senhaLord = '123456A';
-//Acesso como Gestor e Operador
-export const userName = '[data-test="login-input-user_name"]';
-export const userPassword = '[data-test="login-input-password"]';
-export const btnLogin = '[data-test="login-button-login"]';
-export const emailGestor = 'gestorsandra@gestor.com';
-export const senhaGestor = '123mudar';
-export const emailOperador ='operadorsandra@operador.com';
-export const senhaOperador = '123mudar';
+// //Acesso como Lord
+// export const userNameLord = '[data-test="wolologin-input-user_name"]';
+// export const userPasswordLord = '[data-test="wolologin-input-password"]';
+// export const btnLoginLord = '[data-test="wolologin-input-login"]';
+// export const emailLord = 'lord@email.com';
+// export const senhaLord = '123456A';
+// //Acesso como Gestor e Operador
+// export const userName = '[data-test="login-input-user_name"]';
+// export const userPassword = '[data-test="login-input-password"]';
+// export const btnLogin = '[data-test="login-button-login"]';
+// export const emailGestor = 'gestorsandra@gestor.com';
+// export const senhaGestor = '123mudar';
+// export const emailOperador ='operadorsandra@operador.com';
+// export const senhaOperador = '123mudar';
 
-export default {
-    userNameLord,
-    userPasswordLord,
-    btnLoginLord,
-    userName,
-    userPassword,
-    btnLoginLord
+// export default {
+//     userNameLord,
+//     userPasswordLord,
+//     btnLoginLord,
+//     userName,
+//     userPassword,
+//     btnLoginLord
+// }
+
+export const ELEMENTS = {
+    userName:'[data-test="login-input-user_name"]',
+    userPassword: '[data-test="login-input-password"]',
+    btnLogin: '[data-test="login-button-login"]',
+    emailGestor: 'gestorsandra@gestor.com',
+    senhaGestor: '123mudar',
+    emailOperador: 'operadorsandra@operador.com',
+    senhaOperador: '123mudar'
 }
--- cypress/support/pages/login.js
+++ cypress/support/pages/login.js
@@ -1,12 +1,12 @@
-import { userPassword, btnLogin,userName, userNameLord, userPasswordLord, btnLoginLord, emailOperador, senhaOperador } from '../elements/login.js';
 import {baseUrl, baseUrlLogado, baseUrlLord} from '../elements/urls_acessos'
+const el = require('../elements/login.js');
 
 class Login{
    loginGestor(){
     cy.visit(baseUrl)
-    cy.get(userName).type(emailGestor);   
-    cy.get(userPassword).type(senhaGestor);
-    cy.get(btnLogin).click();
+    cy.get(el.userName).type(el.emailGestor);   
+    cy.get(el.userPassword).type(el.senhaGestor);
+    cy.get(el.btnLogin).click();
     cy.wait(2000);
     cy.url().should('contains', baseUrlLogado);
    }
@@ -23,29 +23,13 @@ class Login{
 
    loginOperador(){
     cy.visit(baseUrl);
-    cy.get(userName).type(emailOperador);   
-    cy.get(userPassword).type(senhaOperador);
-    cy.get(btnLogin).click();
+    cy.get(el.userName).type(el.emailOperador);   
+    cy.get(el.userPassword).type(el.senhaOperador);
+    cy.get(el.btnLogin).click();
     cy.url().should('contains', baseUrlLogado)
   }
 
 }
-// Login do gestor
-//Cypress.Commands.add("loginGestor", (emailGestor,senhaGestor) => { 
-   
-//})
-
-//Login Lord
-// Cypress.Commands.add("loginLord", (emailLord, senhaLord) => { 
-  
-// }) 
-
-// //Login Operador
-// Cypress.Commands.add("loginOperador", (emailLord, senhaLord) => { 
-    
- 
- 
-// }) 
 
 export default new Login();
 
