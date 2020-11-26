fadcc92f3f5fe4fef0614a25e5f13b33c134aa74
Nara <nara.silva@gruponewway.com.br>
2020-11-26 09:48:03 -0300

    alteracoes

--- cypress/integration/pages_specs/acesso_relatorios.spec.js
+++ cypress/integration/pages_specs/acesso_relatorios.spec.js
@@ -1,45 +1,44 @@
-/// <reference types="cypress" />
-import { baseUrl } from '../../support/elements/urls_acessos.js';
+///// <reference types= "cypress" />
 
+import Login from '../../support/pages/login.js'
 
 describe('Envio de Ativo pelo sistema como operador - Pela tela inicial', () => {
 
    beforeEach(() => {
 
-    cy.visit(baseUrl);
-    cy.loginOperador();
+    Login.loginGestor();
    })
-    it('Extrair os relatorios do MktZap', () => {
 
+    it('Extrair o relatorio analitico do MktZap', () => {
+
+        cy.url('https://qa-poli.infranw.com.br/reports/analytic');
         cy.get('"data-test="menu-a-report"').click();
-        //clica no btn "relatorios"
-        
-        //-->extracao do relatorio analitico
         cy.get("data-test=menu-a-report_bi_complete").click();
-        //clica no btn "relatorio completo novo"
-        //alinhar o que seria inserido aqui
         cy.get("data-test=menu-a-report_analytic").click();
-        //clica no bnt "relatorio analitico"
         cy.get("vm.utils.checkPeriodReports(90, 'analytic', vm ,vm.download)").click();
-        //faz o download do relatorio analitico
+    })
+
+    it('Extrair o relatorio efetividade do MktZap', () => {
         
-        //--> extracao do relatorio de efetividade
+        cy.url('')
         cy.get('data-test="menu-a-report_effectiviness"').click();
-        //clique no menu - efetividade (aqui ja mostra o periodo atual exibido em tela)
         cy.get('ng-click="vm.downloadCsv()"').click();
-        //clica no btn de baixar o csv
         cy.get('data-test="menu-a-report_productivity"').click();
-        
-        //-->extracao do relatorio de produtividade
-        cy.get('ng-model="vm.filters.type"').select(aqui precisa inserir a insp do campo de selecao)
+    })
+
+    it('Extrair o relatorio efetividade do MktZap', () => {
+
+        cy.get('ng-model="vm.filters.type"').select('aqui precisa inserir a insp do campo de selecao')
         //clica no tipo para selecionar a opcao
         cy.get('')
-
         //extracao do relatorio de enquetes
         cy.get('data-test="menu-a-report_polls"')
         //clica no btn de abrir o relatorio de enquetes
         cy.get('ng-model="vm.poll"').select()
+    
+
+    })
 
 
 
-
+})
--- cypress/support/elements/urls_acessos.js
+++ cypress/support/elements/urls_acessos.js
@@ -6,3 +6,5 @@ export const baseUrlLogado ='https://qa-poli.infranw.com.br/chat';
 export const baseUrlLogout = 'https://qa-poli.infranw.com.br/login';
 export const baseUrlMaling = 'https://qa-poli.infranw.com.br/mailing-list';
 export const baseUrlCampanha = 'https://qa-poli.infranw.com.br/campaign-list';
+export const baseUrlRelatorioAnalitico = 'https://qa-poli.infranw.com.br/reports/analytic';
+export const baseUrlRelatorioEfetividade = 'https://qa-poli.infranw.com.br/reports/effectiviness';
