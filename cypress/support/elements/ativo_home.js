87a79260bb62aafeeeb6987b1da05c13709be7a9
sandra.pavan@gruponewway.com.br <sandra.pavan@gruponewway.com.br>
2020-12-03 10:29:49 -0300

    atualização dos elementos

--- cypress/support/elements/ativo_home.js
+++ cypress/support/elements/ativo_home.js
@@ -9,7 +9,6 @@ export const ELEMENTS_ATIVO_HOME = {
     textoMensagem: 'Testes cypress envio ativo operador',
     criarAtivo: '[ga-event="mktzap_bt_action_attendance_modal_newwpp_modal_create"]',
     spanMensagemSucesso: '[data-test="toast-span-message"]',
-    mensagemSucesso: 'Mensagem enviada com sucesso!',
     responsavel: '#responsible',
     campoHsm: '#activeHsmModel',
     fileInputElement: 'input[name=file]',
