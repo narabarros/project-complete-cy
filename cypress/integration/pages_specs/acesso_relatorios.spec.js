44a38d44445318bf8226c68e29493cf4365d0f97
sandra.pavan@gruponewway.com.br <sandra.pavan@gruponewway.com.br>
2020-12-15 11:12:41 -0300

    alterações visualizadas

--- cypress/integration/pages_specs/acesso_relatorios.spec.js
+++ cypress/integration/pages_specs/acesso_relatorios.spec.js
@@ -1,4 +1,4 @@
-///// <reference types="cypress" />
+/// <reference types="cypress" />
 
 import Login from '../../support/pages/login.js';
 const elBaseUrl = require ('../../support/elements/urls_acessos.js');
@@ -11,13 +11,7 @@ describe('Acesso a relatorios', () => {
         Login.loginGestor();
     })
 
-    //it('Acesso ao relatorio novo', () => {
-        //cy.visit (elBaseUrl.ELEMENTS_BASE_URL.baseUrlRelatorioCompleto);
-        //cy.wait(500000);
-        //cy.get (elRelatorios.ELEMENTS_RELATORIOS.botaoCanalRelatorioCompleto).click();
-    })
-
-    it.only('Acesso ao relatorio analitico', () => {
+    it('Acesso ao relatorio analitico', () => {
         cy.visit (elBaseUrl.ELEMENTS_BASE_URL.baseUrlRelatorioAnalitico);
         cy.get (elRelatorios.ELEMENTS_RELATORIOS.botaoGerarRelatorioAnalitico).click();
     })
@@ -28,21 +22,21 @@ describe('Acesso a relatorios', () => {
         cy.get (elRelatorios.ELEMENTS_RELATORIOS.botaoBaixarRelatorioEfetividade).click();
     })
 
-    it('Acesso ao relatorio de produtividade', () => {
+    it.only('Acesso ao relatorio de produtividade', () => {
         cy.visit (elBaseUrl.ELEMENTS_BASE_URL.baseUrlRelatorioProdutividade);
-        cy.get (elRelatorios.ELEMENTS_RELATORIOS.cxdeTipoRelatorioProd).select('value="VO"');
+        cy.get (elRelatorios.ELEMENTS_RELATORIOS.cxdeTipoRelatorioProd).select('VO');
         cy.get (elRelatorios.ELEMENTS_RELATORIOS.botaoExtrairRelatorioProdutividade).click();
     })
 
     it('Acesso ao relatorio de gatilhos', () => {
         cy.visit (elBaseUrl.ELEMENTS_BASE_URL.baseUrlRelatorioGatilho);
-        cy.get (elRelatorios.ELEMENTS_RELATORIOS.cxCanalRelatorioGatilho).select('WhatsApp');
+        cy.get (elRelatorios.ELEMENTS_RELATORIOS.cxCanalRelatorioGatilho).select('waweb-30');
         cy.get (elRelatorios.ELEMENTS_RELATORIOS.botaoFiltrarRelatorioGatilho).click();
     })
 
     it('Acesso ao relatorio de enquetes', () => {
         cy.visit (elBaseUrl.ELEMENTS_BASE_URL.baseUrlRelatorioEnquete);
-        cy.get (elRelatorios.ELEMENTS_RELATORIOS.cxSelecionarEnquete).select('TesteEnquete');
+        cy.get (elRelatorios.ELEMENTS_RELATORIOS.cxSelecionarEnquete).select('18');
         cy.get (elRelatorios.ELEMENTS_RELATORIOS.botaoVisualizarRelatorioEnquetes).click();
     })
 
@@ -59,7 +53,7 @@ describe('Acesso a relatorios', () => {
 
     it('Acesso ao relatorio de controle de ativos', () => {
         cy.visit (elBaseUrl.ELEMENTS_BASE_URL.baseUrlRelatorioAtivo);
-        cy.get (elRelatorios.ELEMENTS_RELATORIOS.cxCanalRelatorioAtivo).select('Whats QA');
+        cy.get (elRelatorios.ELEMENTS_RELATORIOS.cxCanalRelatorioAtivo).select('number:30');
         cy.get (elRelatorios.ELEMENTS_RELATORIOS.botaoRelatorioCompletoRelatorioAtivo);
     })
 
@@ -67,3 +61,5 @@ describe('Acesso a relatorios', () => {
         cy.visit (elBaseUrl.ELEMENTS_BASE_URL.baseUrlRelatorioOptin);
         cy.get (elRelatorios.ELEMENTS_RELATORIOS.botaoGerarRelatorioOptin);
     })
+
+})
