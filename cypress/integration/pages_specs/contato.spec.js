a47183e5bf6952f927b79d5674f279ff19dfb384
sandra.pavan@gruponewway.com.br <sandra.pavan@gruponewway.com.br>
2021-01-12 15:47:59 -0300

    finalização da tela contato, como editar e detalhe

--- cypress/integration/pages_specs/contato.spec.js
+++ cypress/integration/pages_specs/contato.spec.js
@@ -7,34 +7,26 @@ import cardAtendimento from '../../support/pages/cardAtendimento.js';
 const elContato = require('../../support/elements/contato.js');
 const elAtivo = require('../../support/elements/ativo.js');
 
-describe('Tela de contato', () => {
+describe('Tela de contato - Envio de ativo', () => {
 
     beforeEach(() => {
         Login.loginOperador();
         cy.get(elContato.ELEMENTS_CONTATO.acessaContato).click();
         cy.get(elContato.ELEMENTS_CONTATO.acessaLista).click();
-
-    })
-
-    it('Enviar um ativo sem responsavel para Whatsapp Web', () => {
         cy.get(elContato.ELEMENTS_CONTATO.campoBusca).type(elContato.ELEMENTS_CONTATO.contatoBusca);
         cy.get(elContato.ELEMENTS_CONTATO.botaoBuscar).click();
         cy.wait(1000);
         cy.get(elContato.ELEMENTS_CONTATO.botaoAcoes).click({ force: true });
         cy.get(elContato.ELEMENTS_CONTATO.botaoEnviar).click({ multiple: true });
         cy.wait(1000);
+    })
+    it('Enviar um ativo sem responsavel para Whatsapp Web', () => {
         cy.get(elAtivo.ELEMENTS_ATIVO.canalEnvioWhats).select(elAtivo.ELEMENTS_ATIVO.canalWhats);
         cy.get(elAtivo.ELEMENTS_ATIVO.campoMensagemWhats).type(elAtivo.ELEMENTS_ATIVO.textoMensagem);
         cy.get(elAtivo.ELEMENTS_ATIVO.criarAtivo).click();
         cy.get(elAtivo.ELEMENTS_ATIVO.spanMensagemSucesso).should('have.text', 'Mensagem enviada com sucesso!');
     })
     it('Enviar um ativo sem responsavel para Whatsapp Web', () => {
-        cy.get(elContato.ELEMENTS_CONTATO.campoBusca).type(elContato.ELEMENTS_CONTATO.contatoBusca);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoBuscar).click();
-        cy.wait(1000);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoAcoes).click({ force: true });
-        cy.get(elContato.ELEMENTS_CONTATO.botaoEnviar).click({ multiple: true });
-        cy.wait(1000);
         cy.get(elAtivo.ELEMENTS_ATIVO.canalEnvioWhats).select(elAtivo.ELEMENTS_ATIVO.canalWhats);
         cy.get(elAtivo.ELEMENTS_ATIVO.campoMensagemWhats).type(elAtivo.ELEMENTS_ATIVO.textoMensagem);
         cy.get(elAtivo.ELEMENTS_ATIVO.fileInputElement)
@@ -43,12 +35,6 @@ describe('Tela de contato', () => {
         cy.get(elAtivo.ELEMENTS_ATIVO.spanMensagemSucesso).should('have.text', 'Mensagem enviada com sucesso!');
     })
     it('Enviar um ativo com responsavel para Whatsapp Web', () => {
-        cy.get(elContato.ELEMENTS_CONTATO.campoBusca).type(elContato.ELEMENTS_CONTATO.contatoBusca);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoBuscar).click();
-        cy.wait(1000);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoAcoes).click({ force: true });
-        cy.get(elContato.ELEMENTS_CONTATO.botaoEnviar).click({ multiple: true });
-        cy.wait(1000);
         cy.get(elAtivo.ELEMENTS_ATIVO.canalEnvioWhats).select(elAtivo.ELEMENTS_ATIVO.canalWhats);
         cy.get(elAtivo.ELEMENTS_ATIVO.responsavel).click({ force: true });
         cy.get(elAtivo.ELEMENTS_ATIVO.campoMensagemWhats).type(elAtivo.ELEMENTS_ATIVO.textoMensagem);
@@ -56,12 +42,6 @@ describe('Tela de contato', () => {
         cy.get(elAtivo.ELEMENTS_ATIVO.spanMensagemSucesso).should('have.text', 'Mensagem enviada com sucesso!');
     })
     it('Enviar um ativo com responsavel para Whatsapp Web', () => {
-        cy.get(elContato.ELEMENTS_CONTATO.campoBusca).type(elContato.ELEMENTS_CONTATO.contatoBusca);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoBuscar).click();
-        cy.wait(1000);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoAcoes).click({ force: true });
-        cy.get(elContato.ELEMENTS_CONTATO.botaoEnviar).click({ multiple: true });
-        cy.wait(1000);
         cy.get(elAtivo.ELEMENTS_ATIVO.canalEnvioWhats).select(elAtivo.ELEMENTS_ATIVO.canalWhats);
         cy.get(elAtivo.ELEMENTS_ATIVO.responsavel).click({ force: true });
         cy.get(elAtivo.ELEMENTS_ATIVO.campoMensagemWhats).type(elAtivo.ELEMENTS_ATIVO.textoMensagem);
@@ -70,28 +50,14 @@ describe('Tela de contato', () => {
         cy.get(elAtivo.ELEMENTS_ATIVO.criarAtivo).click();
         cy.get(elAtivo.ELEMENTS_ATIVO.spanMensagemSucesso).should('have.text', 'Mensagem enviada com sucesso!');
     })
-
     it('Enviar um ativo sem responsavel para Enterprise', () => {
-        cy.get(elContato.ELEMENTS_CONTATO.campoBusca).type(elContato.ELEMENTS_CONTATO.contatoBusca);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoBuscar).click();
-        cy.wait(1000);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoAcoes).click({ force: true });
-        cy.get(elContato.ELEMENTS_CONTATO.botaoEnviar).click({ multiple: true });
-        cy.wait(1000);
         cy.get(elAtivo.ELEMENTS_ATIVO.canalEnvioWhats).select(elAtivo.ELEMENTS_ATIVO.canalEnterprise);
         cy.get(elAtivo.ELEMENTS_ATIVO.campoHsm).select(elAtivo.ELEMENTS_ATIVO.hsmEnterprise);
         cy.wait(3000);
         cy.get(elAtivo.ELEMENTS_ATIVO.criarAtivo).click();
         cy.get(elAtivo.ELEMENTS_ATIVO.spanMensagemSucesso).should('have.text', 'Mensagem enviada com sucesso!');
     })
-
     it('Enviar um ativo com responsavel para Enterprise', () => {
-        cy.get(elContato.ELEMENTS_CONTATO.campoBusca).type(elContato.ELEMENTS_CONTATO.contatoBusca);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoBuscar).click();
-        cy.wait(1000);
-        cy.get(elContato.ELEMENTS_CONTATO.botaoAcoes).click({ force: true });
-        cy.get(elContato.ELEMENTS_CONTATO.botaoEnviar).click({ multiple: true });
-        cy.wait(1000);
         cy.get(elAtivo.ELEMENTS_ATIVO_HOME.canalEnvioWhats).select(elAtivo.ELEMENTS_ATIVO.canalEnterprise);
         cy.get(elAtivo.ELEMENTS_ATIVO_HOME.responsavel).click({ force: true });
         cy.get(elAtivo.ELEMENTS_ATIVO_HOME.campoHsm).select(elAtivo.ELEMENTS_ATIVO.hsmEnterprise);
@@ -105,5 +71,35 @@ describe('Tela de contato', () => {
         cardAtendimento.finalizacaoCardStatus();
     })
 
+})
+
+describe('Tela Contato - Editar/Detalhes', () => {
+    before(() => {
+        Login.loginOperador();
+        cy.get(elContato.ELEMENTS_CONTATO.acessaContato).click();
+        cy.get(elContato.ELEMENTS_CONTATO.acessaLista).click();
+        cy.get(elContato.ELEMENTS_CONTATO.campoBusca).type(elContato.ELEMENTS_CONTATO.contatoBusca);
+        cy.get(elContato.ELEMENTS_CONTATO.botaoBuscar).click();
+        cy.wait(1000);
+    })
+    it('Cancelar a edição do contato', () => {
+        cy.get(elContato.ELEMENTS_CONTATO.botaoAcoes).click({ force: true });
+        cy.get(elContato.ELEMENTS_CONTATO.botaoEditar).click({ multiple: true });
+        cy.get(elContato.ELEMENTS_CONTATO.campoNome).clear().type(elContato.ELEMENTS_CONTATO.textoCampoNome);
+        cy.get(elContato.ELEMENTS_CONTATO.botaoCancelar).click();
+        cy.get(elContato.ELEMENTS_CONTATO.modal).should('not.be.disabled');
+    })
+    it('Editar o contato', () => {
+        cy.get(elContato.ELEMENTS_CONTATO.botaoAcoes).click({ force: true });
+        cy.get(elContato.ELEMENTS_CONTATO.botaoEditar).click({ multiple: true });
+        cy.get(elContato.ELEMENTS_CONTATO.campoNome).clear().type(elContato.ELEMENTS_CONTATO.textoCampoNome);
+        cy.get(elContato.ELEMENTS_CONTATO.botaoSalvar).click();
+        cy.get(elAtivo.ELEMENTS_ATIVO.spanMensagemSucesso).should('have.text', 'Contato alterado com sucesso.');
+    })
+    it('Detalhes do contato', () => {
+        cy.get(elContato.ELEMENTS_CONTATO.botaoDetalhe).click();
+        cy.get(elContato.ELEMENTS_CONTATO.botaoCancelar).click();
+        cy.get(elContato.ELEMENTS_CONTATO.modal).should('not.be.disabled');
+    })
 })
 
--- cypress/support/elements/contato.js
+++ cypress/support/elements/contato.js
@@ -5,5 +5,12 @@ export const  ELEMENTS_CONTATO = {
     botaoBuscar: '#contactlist_btnSearchContact',
     botaoAcoes: '[ga-event="mktzap_bt_dropdown_contact"]',
     botaoEnviar: '[ga-event="mktzap_bt_active_contact"]',
-    contatoBusca: '14997142480'
+    contatoBusca: '5514997142480',
+    botaoEditar: '[ga-event="mktzap_bt_edit_contact"]',
+    campoNome: '#contact_fldName',
+    botaoSalvar: '#contact_btnSaveContact',
+    botaoCancelar: '#contact_btnCancelContactEdit',
+    modal: '#personEdit',
+    botaoDetalhe:'[ga-event="mktzap_bt_view_contact"]',
+    textoCampoNome: 'Sandra'
 }
