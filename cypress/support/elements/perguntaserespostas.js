dbd44c06301f09e221b1a5bb5d6719830c715d85
Nara <nara.silva@gruponewway.com.br>
2021-01-21 14:55:23 -0300

    atualizacao

--- cypress/support/elements/perguntaserespostas.js
+++ cypress/support/elements/perguntaserespostas.js
@@ -3,7 +3,7 @@ export const ELEMENTS_PERGUNTASERESPOSTAS = {
 
 btnCriar: '[ga-event="mktzap_bt_action_knowledge_base_create"]',
 cxTitulo: '[data-test="knowledge-base-title-text"]',
-cxTexto: '[ng-model="vm.knowledgeBase.text"]',
+cxTexto: '[data-test="knowledge-base-title-textarea"]',
 btnSalvar: '[ga-event="mktzap_bt_action_knowledge_base_save"]',
 
 }
