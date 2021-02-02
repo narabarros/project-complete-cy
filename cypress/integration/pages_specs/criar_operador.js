e9a845bcd6180d7ed503ded274c9e1025044fee0
Nara <nara.silva@gruponewway.com.br>
2021-02-02 10:48:41 -0300

    completo

---
+++ cypress/integration/pages_specs/criar_operador.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+import Login from '../../support/pages/login.js';
+const elBaseUrl = require ('../../support/elements/urls_acessos.js');
+const elcriaroperador = require ('../../support/elements/criaroperador.js');
+
+
+describe('Criar Operador', () => {
+
+    before(() => {
+        Login.loginGestor();
+})
+
+
+    it('Criar operador', () => {
+        cy.visit (elBaseUrl.ELEMENTS_BASE_URL.baseUrlConfiguracoes);
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.btnConfig).click();
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.btnCriarUsuario).click();
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.txtNome).type('narita');
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.txtEmail).type('naritatest@email.com');
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.txtNomeExibicao).type('Narita');
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.txtSenha).type('123456A');
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.btnFeminino).click();
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.btnFuncao).select('1');
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.btnSalvar).click();
+        cy.get (elcriaroperador.ELEMENTS_CRIAROPERADOR.btnConfirmar).click({ multiple: true });
+
+})
+})
