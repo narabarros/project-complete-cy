6affee733c26a374c7b00271aa5fdcfaf31f6ef5
Nara <nara.silva@gruponewway.com.br>
2021-02-02 13:36:53 -0300

    concluido

---
+++ cypress/integration/pages_specs/criar_setor.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+import Login from '../../support/pages/login.js';
+const elBaseUrl = require ('../../support/elements/urls_acessos.js');
+const elcriarsetor = require ('../../support/elements/criarsetor.js');
+
+
+describe('Criar setor', () => {
+
+    before(() => {
+        Login.loginGestor();
+})
+
+
+    it('Criar setor', () => {
+
+
+        cy.visit(elBaseUrl.ELEMENTS_BASE_URL.baseUrlConfiguracoes);
+        cy.get(elcriarsetor.ELEMENTS_CRIARSETOR.btnSetor).click();
+        cy.get(elcriarsetor.ELEMENTS_CRIARSETOR.cxNomeSetor).type('Teste');
+        cy.get(elcriarsetor.ELEMENTS_CRIARSETOR.btnSalvarSetor).click();
+
+
+
+
+})
+})
---
+++ cypress/support/elements/criarsetor.js
@@ -0,0 +1,9 @@
+export const ELEMENTS_CRIARSETOR = {
+
+    btnSetor: '[data-test="company-tab-sectors"]',
+    cxNomeSetor: '[data-test="company-tab-sector-text-new"]',
+    btnSalvarSetor: '[data-test="company-tab-sector-button-new"]',
+    
+
+
+}
